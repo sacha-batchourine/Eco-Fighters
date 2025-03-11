fecea4755c728c0d4d801ef3df5fcd38a4d5b846
Gabriel Cabooter <gabriel.cabooter@epfedu.fr>
2025-03-11 15:11:26 +0100

    Pleine écran

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import Menu from "./js/menu.js";
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,  // Largeur adaptative à l'écran
+    height: window.innerHeight, // Hauteur adaptative à l'écran
     physics: {
-        default: 'arcade',
-        arcade: { debug: false }
+        default: "arcade",
+        arcade: { 
+            gravity: { y: 300 }, 
+            debug: false 
+        }
     },
-    scene: [Menu, Hub]  // On définit les scènes ici, dans l'ordre
+    scale: {
+        mode: Phaser.Scale.FIT,  // Ajuste le jeu à la fenêtre sans distorsion
+        autoCenter: Phaser.Scale.CENTER_BOTH  // Centre le jeu dans la fenêtre
+    },
+    scene: [Menu, Hub]  // Définition des scènes
 };
 
+// Création et lancement du jeu
 const game = new Phaser.Game(config);
+game.scene.start("Menu");  // Lancement de la scène "Menu"
--- src/js/Hub.js
+++ src/js/Hub.js
@@ -1,4 +1,4 @@
-export default class niveau1 extends Phaser.Scene {
+export default class Hub extends Phaser.Scene {
     constructor() {
       super({ key: "Hub" });
     }
