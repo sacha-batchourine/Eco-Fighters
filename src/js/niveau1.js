550134961924e78ae8ea120ee62817c4b3c1a85f
Gabriel Cabooter <gabriel.cabooter@epfedu.fr>
2025-03-11 12:46:18 +0100

    test

--- src/js/niveau1.js
+++ src/js/niveau1.js
@@ -4,7 +4,7 @@ export default class niveau1 extends Phaser.Scene {
     // constructeur de la classe
     constructor() {
       super({
-        key: "niveau1" //  ici on précise le nom de la classe en tant qu'identifiant
+        key: "niveau1" //  ici on précise le nom de la classe en tant qu'identifiant TESTEEEEEEEE
       });
     }
     preload() {}
