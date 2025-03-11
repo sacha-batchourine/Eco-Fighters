598b2bdf832993444730296bf7ff2e57858144a0
Batchourine <sacha.batchourine@epfedu.fr>
2025-03-11 12:43:51 +0100

    modif affichage tiled

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
-// Importation des scènes
-import Menu from './menu.js';
-import Hub from './Hub.js';
-
-// Configuration du jeu
+import Phaser from 'phaser';
+import Hub from "./js/Hub.js"; // Si Hub.js se trouve dans le dossier js
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 },
-      debug: false
-    }
+      default: 'arcade',
+      arcade: { debug: false }
   },
-  scene: [Menu, Hub], // Ajoute les scènes
+  scene: [Hub]
 };
 
-// Création du jeu
 const game = new Phaser.Game(config);
