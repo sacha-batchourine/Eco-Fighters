a4a59be499409e46782adca13894481975799832
Gabriel Cabooter <gabriel.cabooter@epfedu.fr>
2025-03-12 22:15:48 +0100

    Niveau2 qui marche

--- src/js/niveau2.js
+++ src/js/niveau2.js
@@ -4,8 +4,7 @@ export default class Niveau2 extends Phaser.Scene {
         this.maxHealth = 5;
         this.currentHealth = this.maxHealth;
         this.lastDirection = "right"; // Dernière direction du joueur
-        this.fireRate = 500; // Temps entre chaque tir (ms)
-        this.lastFired = 0; // Temps du dernier tir
+        
     }
 
     preload() {
@@ -60,13 +59,18 @@ export default class Niveau2 extends Phaser.Scene {
         // CREATION DE TOUCHE
         this.cursors = this.input.keyboard.createCursorKeys();
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.shootKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         
+        //COLISIONS
         mursLayer.setCollisionByProperty({ estSolide: true });
         this.physics.add.collider(this.player, mursLayer);
         
+
+        //PORTAIL
         this.portal = this.physics.add.sprite(3025, 525, "portail").setImmovable(true);
         this.physics.add.overlap(this.player, this.portal, this.onPortalOverlap, null, this);
 
+        //BURGER
         this.anims.create({
             key: "burger_left",
             frames: this.anims.generateFrameNumbers("burger", { frames: [6, 7, 10, 11] }),
@@ -81,7 +85,7 @@ export default class Niveau2 extends Phaser.Scene {
             repeat: -1
         });
 
-        // Création des ennemis (les burgers)
+        
         this.burgers = this.physics.add.group({ key: 'burger', repeat: 14 });
         this.burgers.children.iterate(burger => {
             let x = Phaser.Math.Between(50, map.widthInPixels - 50);
@@ -102,6 +106,8 @@ export default class Niveau2 extends Phaser.Scene {
         // Détection des collisions entre le joueur et les burgers
         this.physics.add.collider(this.player, this.burgers, this.hitPlayer, null, this);
 
+
+        //VIE
         this.healthIcons = [];
         for (let i = 0; i < this.maxHealth; i++) {
             let heart = this.add.image(60 + i * 50, 20, "heart").setScale(0.3).setScrollFactor(0);
@@ -109,6 +115,7 @@ export default class Niveau2 extends Phaser.Scene {
         }
         this.updateHealth();
 
+        //CAMERA
         this.cameras.main.startFollow(this.player);
         this.cameras.main.setZoom(1.1);
         this.cameras.main.setBounds(-50, -25, map.widthInPixels + 50, map.heightInPixels);
@@ -116,17 +123,64 @@ export default class Niveau2 extends Phaser.Scene {
 
     update(time) {
         let speed = 160;
-        let moving = false;
+        let diagonalSpeed = Math.sqrt(speed * speed / 2); // Réduit la vitesse en diagonale
+        
+        let movingX = false;
+        let movingY = false;
+        
+        // Mouvement horizontal
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-speed);
+            this.player.anims.play("walk_right", true);
+            this.player.setFlipX(true);
+            this.lastDirection = "left";
+            movingX = true;
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(speed);
+            this.player.anims.play("walk_right", true);
+            this.player.setFlipX(false);
+            this.lastDirection = "right";
+            movingX = true;
+        } else {
+            this.player.setVelocityX(0);
+        }
+        
+        // Mouvement vertical
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-speed);
+            movingY = true;
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(speed);
+            movingY = true;
+        } else {
+            this.player.setVelocityY(0);
+        }
+        
+        // Gestion des animations pour le mouvement vertical
+        if (movingY && !movingX) {
+            if (this.lastDirection === "right") {
+                this.player.anims.play("walk_right", true);
+                this.player.setFlipX(false);
+            } else if (this.lastDirection === "left") {
+                this.player.anims.play("walk_right", true);
+                this.player.setFlipX(true);
+            }
+        }
+        
+        // Si on bouge en diagonale, on ajuste la vitesse
+        if (movingX && movingY) {
+            this.player.setVelocityX(this.player.body.velocity.x * diagonalSpeed / speed);
+            this.player.setVelocityY(this.player.body.velocity.y * diagonalSpeed / speed);
+        }
+        
+        // Si le joueur ne bouge pas, animation d'arrêt
+        if (!movingX && !movingY) {
+            this.player.anims.play("stand", true);
+        }
 
-        if (this.cursors.left.isDown) { this.player.setVelocityX(-speed); moving = true; }
-        else if (this.cursors.right.isDown) { this.player.setVelocityX(speed); moving = true; }
-        else { this.player.setVelocityX(0); }
-
-        if (this.cursors.up.isDown) { this.player.setVelocityY(-speed); moving = true; }
-        else if (this.cursors.down.isDown) { this.player.setVelocityY(speed); moving = true; }
-        else { this.player.setVelocityY(0); }
-
-        if (!moving) { this.player.anims.stop(); }
+        if (Phaser.Input.Keyboard.JustDown(this.shootKey)) {
+            this.tirer();
+        }
 
         // Logique des burgers
         this.burgers.children.iterate(burger => {
@@ -136,20 +190,13 @@ export default class Niveau2 extends Phaser.Scene {
             burger.play(burger.body.velocity.x > 0 ? "burger_right" : "burger_left", true);
         });
 
-        // Tir du joueur
-        if (Phaser.Input.Keyboard.JustDown(this.shootKey) && time > this.lastFired + this.fireRate) {
-            this.shoot();
-            this.lastFired = time;
-        }
     }
 
     // Fonction pour tirer un projectile
-    shoot() {
+    tirer() {
         let bullet = this.bullets.create(this.player.x, this.player.y, "bullet");
         bullet.setScale(0.5);
         bullet.setVelocityX(this.lastDirection === "right" ? 300 : -300);
-
-        // Délai avant de détruire le projectile
         this.time.addEvent({
             delay: 2000,
             callback: () => bullet.destroy(),
@@ -166,18 +213,17 @@ export default class Niveau2 extends Phaser.Scene {
         }
         burger.setActive(false).setVisible(false);
     }
+    hitPlayer(player, burger) {
+        this.currentHealth -= 1;
+        this.updateHealth();
+        if (this.currentHealth <= 0) { this.scene.restart(); }
+        burger.setActive(false).setVisible(false);
+    }
     hitBurger(bullet, burger) {
         bullet.destroy();
         burger.destroy();
     }
 
-    // Fonction pour détruire un burger quand il est touché par un projectile
-    hitBurger(bullet, burger) {
-        bullet.destroy();
-        burger.destroy();
-    }
-
-    // Fonction pour mettre à jour les icônes de vie
     updateHealth() {
         this.healthIcons.forEach((heart, index) => {
             heart.setVisible(index < this.currentHealth);
